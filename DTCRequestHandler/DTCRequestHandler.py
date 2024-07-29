761fac994bd08d27ddb3f2c89fb0265df13780ee
ValerianCoelho <valerian27012004@gmail.com>
2024-07-29 12:50:27 +0530

    class Receive changed to class ResponseManager

--- DTCRequestHandler/DTCRequestHandler.py
+++ DTCRequestHandler/DTCRequestHandler.py
@@ -28,6 +28,12 @@ class PCAN:
         else:
             print("CAN message transmitted successfully")
     
+    def receive_frame(self) -> None:
+        # write logic to get the next frame on the Receive Queue
+        result, msg, timestamp = self.pcan.Read(self.channel)
+        # print(msg)
+        return tuple(msg.DATA)
+    
     def equals(self, msg1, msg2):
         return msg1 == msg2
     
@@ -59,15 +65,9 @@ class Idle:
         # write logic to send DTC Req (0x19) and change state to Receive
         self.pcan.send_frame(0x743, (0x03, 0x19, 0x02, 0x09, 0x00, 0x00, 0x00, 0x00))
 
-class Receive:
+class ResponseManager:
     def __init__(self, pcan) -> None:
         self.pcan = pcan
-
-    def receive_frame(self) -> None:
-        # write logic to get the next frame on the Receive Queue
-        result, msg, timestamp = self.pcan.pcan.Read(self.pcan.channel)
-        # print(msg)
-        return tuple(msg.DATA)
 
     def send_control_frame(self) -> None:
         # write logic to send a control frame
@@ -87,7 +87,7 @@ class DTCRequestHandler:
         self.pcan = PCAN()  # Initialize PCAN instance
         self.inactive = Inactive(self.pcan)
         self.idle = Idle(self.pcan)
-        self.receive = Receive(self.pcan)
+        self.response_manager = ResponseManager(self.pcan)
 
         self.p2: float = 0.0 # in miliseconds
         self.p2_star: float = 0.0 # in miliseconds
@@ -105,7 +105,7 @@ class DTCRequestHandler:
 
         while True:
             time.sleep(0.1)
-            received_frame = self.receive.receive_frame()
+            received_frame = self.pcan.receive_frame()
             # if I didn't receive RCRPRP frame then I must have gotten correct frame. although 
             # this won't work if there are multiple frames that I can receive
             print(self.pcan.hex(received_frame))
@@ -127,7 +127,7 @@ class DTCRequestHandler:
         start_time = time.time()
         while time.time() - start_time < self.pcan.seconds(self.p2_star):
             time.sleep(self.pcan.seconds(self.p2))
-            received_frame = self.receive.receive_frame()
+            received_frame = self.pcan.receive_frame()
             # if I didn't receive RCRPRP frame then I must have gotten correct frame. although 
             # this won't work if there are multiple frames that I can receive
             print(self.pcan.hex(received_frame))
@@ -135,6 +135,8 @@ class DTCRequestHandler:
             if not self.pcan.equals(received_frame, RCRPRP) and not self.pcan.equals(received_frame, negative):
                 break
         print("FF", self.pcan.hex(received_frame))
+
+        
 
     def handle_request(self) -> None:
         # Example of handling request based on the current state
@@ -144,7 +146,7 @@ class DTCRequestHandler:
             self.idle.send_DTC_request()
             self.set_state(DTCRequestHandler.RECEIVE)
         elif self.state == DTCRequestHandler.RECEIVE:
-            self.receive.receive_frame()
+            self.pcan.receive_frame()
             # Additional handling logic for Receive state
 
 # Example usage
