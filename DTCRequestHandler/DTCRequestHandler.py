c1fc62594a131e66996da18bf5c62b4847a013d0
ValerianCoelho <valerian27012004@gmail.com>
2024-07-31 11:59:29 +0530

    Merged state classes into DTCRequestHandler class

--- DTCRequestHandler/DTCRequestHandler.py
+++ DTCRequestHandler/DTCRequestHandler.py
@@ -5,11 +5,9 @@ from . import PCANBasic
 from .UDSException import UDSException
 from .PCANConstants import PCAN_CHANNELS, PCAN_BAUD_RATES
 
-
 class PCAN:
     def __init__(self, channel, baud) -> None:
         self.pcan = PCANBasic.PCANBasic()  # Initialize the PCANBasic instance
-        # self.channel = PCANBasic.PCAN_USBBUS1  # Define the PCAN channel you are using (e.g., PCAN_USBBUS1 for the first USB channel)
         self.channel = PCAN_CHANNELS[channel]  # Define the PCAN channel you are using (e.g., PCAN_USBBUS1 for the first USB channel)
         self.baudrate = PCAN_BAUD_RATES[baud]  # Define the baud rate (e.g., PCAN_BAUD_500K for 500 kbps)
         self.pcan_channel = self.pcan.Initialize(self.channel, self.baudrate)  # Initialize the PCAN channel
@@ -45,9 +43,7 @@ class PCAN:
         if current > end:
             current = start + (current - end - 1)
         return current
-        
 
-    
     def equals(self, msg1, msg2):
         return msg1 == msg2
     
@@ -56,43 +52,6 @@ class PCAN:
     
     def seconds(self, ms):
         return ms / 1000.0
-
-class Inactive:
-    def __init__(self, pcan, frame) -> None:
-        self.pcan = pcan
-        self.frame = frame
-
-    def send_start_session_frame(self) -> None:
-        self.pcan.send_frame(Frame.ARBITRATION_ID, Frame.SESSION_START_REQ)
-
-    def extract_time(self, msg):
-        p2 = msg[3]
-        p2_star = msg[4:7]
-        p2_star = (p2_star[0] << 16) | (p2_star[1] << 8) | p2_star[2]
-        return p2, p2_star
-
-class Idle:
-    def __init__(self, pcan, frame) -> None:
-        self.pcan = pcan
-        self.frame = frame
-
-    def send_DTC_request(self) -> None:
-        self.pcan.send_frame(Frame.ARBITRATION_ID, Frame.DTC_REQUEST)
-
-class ResponseManager:
-    def __init__(self, pcan, frame) -> None:
-        self.pcan = pcan
-        self.frame = frame
-
-    def send_control_frame(self, block_size, time_between_consecutive_frame) -> None:
-        self.pcan.send_frame(Frame.ARBITRATION_ID, self.frame.construct_flow_control(block_size, time_between_consecutive_frame))
-
-    def send_tester_frame(self) -> None:
-        # write logic to send a control frame
-        pass
-
-    def extract_data_length(self, msg) -> None:
-        return msg[1], list(msg[5:])
 
 class DTCRequestHandler:
     INACTIVE: int = 1
@@ -103,9 +62,6 @@ class DTCRequestHandler:
         self.state = DTCRequestHandler.INACTIVE
         self.pcan = PCAN(channel, baud)  # Initialize PCAN instance
         self.frame = Frame.Frame() # Initialize Frame Instance
-        self.inactive = Inactive(self.pcan, self.frame)
-        self.idle = Idle(self.pcan, self.frame)
-        self.response_manager = ResponseManager(self.pcan, self.frame)
 
         self.block_count = 0x03 # ms
         self.time_between_consecutive_frame = 0x14 # ms
@@ -117,8 +73,24 @@ class DTCRequestHandler:
     def set_state(self, new_state) -> None:
         self.state = new_state
 
+    def extract_time(self, msg):
+        p2 = msg[3]
+        p2_star = msg[4:7]
+        p2_star = (p2_star[0] << 16) | (p2_star[1] << 8) | p2_star[2]
+        return p2, p2_star
+    
+    def send_control_frame(self, block_size, time_between_consecutive_frame) -> None:
+        self.pcan.send_frame(Frame.ARBITRATION_ID, self.frame.construct_flow_control(block_size, time_between_consecutive_frame))
+
+    def send_tester_frame(self) -> None:
+        # write logic to send a control frame
+        pass
+
+    def extract_data_length(self, msg) -> None:
+        return msg[1], list(msg[5:])
+
     def start_session(self):
-        self.inactive.send_start_session_frame()
+        self.pcan.send_frame(Frame.ARBITRATION_ID, Frame.SESSION_START_REQ)
         self.set_state(DTCRequestHandler.RECEIVE)
 
         # Wait till we get positive response
@@ -132,11 +104,11 @@ class DTCRequestHandler:
             else:
                 break
         
-        self.p2, self.p2_star = self.inactive.extract_time(received_frame)
+        self.p2, self.p2_star = self.extract_time(received_frame)
         self.set_state(DTCRequestHandler.IDLE)
     
     def request_for_DTC(self): # sends the frame and combines the response and returns it
-        self.idle.send_DTC_request() # sends the DTC req frame
+        self.pcan.send_frame(Frame.ARBITRATION_ID, Frame.DTC_REQUEST) # sends the DTC req frame
         self.set_state(DTCRequestHandler.RECEIVE)
 
         # start_time = time.time()
@@ -152,9 +124,9 @@ class DTCRequestHandler:
             else:
                 break
         
-        data_length, data = self.response_manager.extract_data_length(received_frame)
+        data_length, data = self.extract_data_length(received_frame)
         for i in range(math.ceil(data_length / (self.block_count * 7))):
-            self.response_manager.send_control_frame(self.block_count, self.time_between_consecutive_frame)
+            self.send_control_frame(self.block_count, self.time_between_consecutive_frame)
 
             while True:
                 time.sleep(self.pcan.seconds(int("14", 16)))
@@ -167,4 +139,3 @@ class DTCRequestHandler:
             self.current_pos = self.pcan.increment(self.current_pos, self.block_count)
             print(hex(self.current_pos))
         print(self.pcan.hex(data))
-
