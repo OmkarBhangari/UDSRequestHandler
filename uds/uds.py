030f791a2c2efa9776296dcc224898a1c11d737a
ValerianCoelho <valerian27012004@gmail.com>
2024-08-02 14:38:33 +0530

    push_frame function added

--- uds/uds.py
+++ uds/uds.py
@@ -1,16 +1,22 @@
 import time
+import queue
 import threading
 
 class UDS:
     def __init__(self):
-        self.main_queue = []
+        self.main_queue = queue.Queue()
 
         event_thread = threading.Thread(target=self.event_loop)
         event_thread.start()
         
     def event_loop(self):
         while True:
-            print("Hello")
+            print(self.main_queue.get())
             time.sleep(1)
 
+    def push_frame(self, frame):
+        self.main_queue.put(frame)
+
 uds = UDS()
+time.sleep(3)
+uds.push_frame(2)
