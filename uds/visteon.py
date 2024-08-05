a4f462041dbcc191b3b979556469a38d53ac7ed1
ValerianCoelho <valerian27012004@gmail.com>
2024-08-05 15:17:58 +0530

    start session and send DTC request functions running on asyncio loop

--- uds/visteon.py
+++ uds/visteon.py
@@ -1,7 +1,8 @@
 import time
 import queue
 import threading
-import tkinter as tk
+import ttkbootstrap as ttk
+import asyncio
 from pcan import PCAN  # Ensure the pcan module is correctly installed
 
 class Tx:
@@ -45,20 +46,37 @@ class UDS:
                 frame = self.queue.get()
                 self.tx.transmit(frame)
                 print(f"Frame {frame} transmitted")
-            print("Hello")
+            
+            received_frame = self.rx.receive()
+            '''
+            try:
+                frame_type = validate_frame(received_frame)
+            except Exception as e:
+                print(e)
 
+            if frame_type == "single frame":
+                extract_data = received_frame
+                if extract_data.recepient == "0x19":
+                    # send data to 0x19 and trigger an event
+                    pass
+                elif extract_data.recepient == "0x19":
+                    # send data to 0x10 and trigger an event
+                    pass
+            '''
+
+            print(received_frame)
             time.sleep(1)
 
     def push_frame(self, frame):
         self.queue.put(frame)
 
 class Ox10:
-    START_SESSION = (0x02, 0x10, 0x03, 0x09, 0x00, 0x00, 0x00, 0x00)
+    START_SESSION = (0x02, 0x10, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00)
 
     def __init__(self, uds):
         self.uds = uds
 
-    def send_start_session_request(self):
+    async def send_start_session_request(self):
         self.uds.push_frame(Ox10.START_SESSION)
 
 class Ox3E:
@@ -76,7 +94,7 @@ class Ox19:
     def __init__(self, uds):
         self.uds = uds
 
-    def send_dtc_request(self):
+    async def send_dtc_request(self):
         self.uds.push_frame(Ox19.DTC_REQUEST)
 
 class GuiInterface:
@@ -88,24 +106,39 @@ class GuiInterface:
         self.ox19 = Ox19(self.uds)
         self.ox10 = Ox10(self.uds)
 
-        self.start_session_button = tk.Button(master, text="Start Session", command=self.start_session)
+        self.start_session_button = ttk.Button(master, text="Start Session", command=self.run_async_function(self.start_session))
         self.start_session_button.pack()
 
-        self.send_dtc_button = tk.Button(master, text="Send DTC Request", command=self.send_dtc_request)
+        self.send_dtc_button = ttk.Button(master, text="Send DTC Request", command=self.run_async_function(self.send_dtc_request))
         self.send_dtc_button.pack()
 
-        self.log = tk.Text(master)
+        self.log = ttk.Text(master)
         self.log.pack()
 
-    def start_session(self):
-        self.ox10.send_start_session_request()
-        self.log.insert(tk.END, "Session started\n")
+    async def start_session(self):
+        await self.ox10.send_start_session_request()
+        self.log.insert(ttk.END, "Session started\n")
 
-    def send_dtc_request(self):
-        self.ox19.send_dtc_request()
-        self.log.insert(tk.END, "DTC Request sent\n")
+    async def send_dtc_request(self):
+        await self.ox19.send_dtc_request()
+        self.log.insert(ttk.END, "DTC Request sent\n")
+
+    def run_async_function(self, async_func):
+        def wrapper():
+            asyncio.run_coroutine_threadsafe(async_func(), asyncio_loop)
+        return wrapper
+
+# Function to run the asyncio event loop
+def start_async_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+# Create an asyncio event loop in a separate thread
+asyncio_loop = asyncio.new_event_loop()
+t = threading.Thread(target=start_async_loop, args=(asyncio_loop,), daemon=True)
+t.start()
 
 # Set up the GUI
-root = tk.Tk()
+root = ttk.Window()
 gui = GuiInterface(root)
 root.mainloop()
