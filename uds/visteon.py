a33e4c52b4e471ee45f8dd6ac16a092241523342
ValerianCoelho <valerian27012004@gmail.com>
2024-08-05 12:40:09 +0530

    class structure defined for program

---
+++ uds/visteon.py
@@ -0,0 +1,23 @@
+from .pcan import PCAN
+
+class Ox10:
+    pass
+class Ox3E:
+    pass
+class Ox22:
+    pass
+class Ox2E:
+    pass
+class Ox19:
+    pass
+
+
+class TP:
+    pass
+class UDS:
+    pass
+
+class Rx:
+    pass
+class Tx:
+    pass
