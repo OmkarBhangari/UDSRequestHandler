01ba6352bf0cce9a129fed739b435815eb8ccd46
ValerianCoelho <valerian27012004@gmail.com>
2024-08-21 12:56:06 +0530

    removed unwanted imports

--- ecupath/__init__.py
+++ ecupath/__init__.py
@@ -1,9 +1,6 @@
 from .uds import UDS
 from .Can import CAN
 from .can_tp import CAN_TP
-#from ..gui import DiagnosticGUI
-""" from .Ox10 import Ox10
-from .Ox19 import Ox19 """
 from .event_manager import EventManager
 
 __all__ = ['UDS', 'CAN', 'CAN_TP', 'EventManager', 'PCAN']
